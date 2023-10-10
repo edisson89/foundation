88429ff07f860848694909913d1f34ac0c7a77a3
Juan Martín Salinas <juanmartinsalinas1@gmail.com>
2023-10-10 17:33:10 -0300

    inicioChanges

--- Client/src/home/home.jsx
+++ Client/src/home/home.jsx
@@ -1,7 +1,17 @@
+import styles from './home.module.css'
+import portada from '../assets/Portada/Portada (1).jpg'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <div className={styles.portadaContainer}>
+            <img className={styles.portadaImagen} src={portada}/>
+        </div>
+        <section className={styles.blog}>
+            <p>XD</p>
+        </section>
+    </div>
   )
 }
 
