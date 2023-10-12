1e5c421c1742dcab8b8ba31f81583a79f7c49f25
edisson89 <edisson.12@hotmail.com>
2023-10-11 22:30:43 -0500

    configurando conexion al  servidor

---
+++ Server/index.js
@@ -0,0 +1,9 @@
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+  })
+}).catch(error => console.error(error))
