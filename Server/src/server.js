5bd871ecb51354c448acc5bd4859854a7086f928
edisson89 <edisson.12@hotmail.com>
2023-10-11 22:31:36 -0500

    inicio app

---
+++ Server/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const server = express();
+
+server.use(morgan("dev"));
+server.use(express.json());
+server.use(cors());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+server.use(router);
+
+module.exports = server;
