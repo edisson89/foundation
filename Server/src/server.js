22fc3b96fd1a73aab7914f5bc2dfd9499af51f63
edisson89 <edisson.12@hotmail.com>
2023-12-01 22:42:17 -0500

    cambios port

--- Server/src/server.js
+++ Server/src/server.js
@@ -10,7 +10,7 @@ server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:80"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
