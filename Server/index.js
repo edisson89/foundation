f76611f669a3444f8e65fb8b084d713a2a09c812
edisson89 <edisson.12@hotmail.com>
2023-12-05 16:59:28 -0500

    ajustando port

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,10 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 require("dotenv").config();
-const {PORT} = process.env
+const {PORT,IP} = process.env
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`);
+    console.log(`Server listening on port http://${IP}:${PORT}`);
   })
 }).catch(error => console.error(error))
